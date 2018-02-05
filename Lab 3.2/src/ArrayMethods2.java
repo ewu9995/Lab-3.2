2900c980a22b3e1e471a37c229db6b275c1ef028
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-02-05 08:23:48 -0500

    .

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -1,11 +1,28 @@
 
 public class ArrayMethods2 {
 
-	public static int[] merge(int[] list1, int[] list2) {
+	public static String[] merge(int[] String, String[] list2) {
+		
+		
+		
+		return String[] x;
 		
 	}
 	
 	public static int partition(int[]list) {
-		
+		int pivotLocation = 0;
+		for(int i=0; i < list.length-1; i++ ) {
+			int pivot = i;
+			pivotLocation = list[i];
+			for(int j = list.length-1; j >0; j-- ) {
+				if(j> pivot) {
+					
+				}
+				else if (pivot >j) {
+					
+				}
+			}
+		}
+		return pivotLocation;
 	}
 }
