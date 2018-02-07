32623c9daa0c77a9cca51c6e943c6f64363ccd26
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-02-07 09:23:06 -0500

    fgrerwerewweewrwew

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -4,7 +4,7 @@ public class ArrayMethods2 {
 
 	public static void main(String[] args) {
 		String[] list1 = {"abc","kab","mqw","dwe"};
-		String[] list2 = {"cqw","ewqe","fsdada","gwew"};
+		String[] list2 = {"cqw","qwqe","sdada","gwew"};
 		int[] list = {5,6,9,1,3,7};
 	
 		long start = System.nanoTime();
@@ -25,19 +25,20 @@ public class ArrayMethods2 {
 	
 	
 
-
-
 //https://stackoverflow.com/questions/5958169/how-to-merge-two-sorted-arrays-into-a-sorted-array
 
 	public static String[] merge(String[] list1 , String[] list2) {
+		String[] combinedArray = null;
 		boolean sorted1 = false, sorted2 = false;
-		String[] combinedArray = new String[list1.length + list2.length];
+		String[] a = null;
+		
 		for(int i = 0; i < list1.length-1; i++) 
 		{
 			if(list1[i].compareTo(list1[i+1])>0) {
 				sorted1 = true;
 			}
 		}
+		
 		for(int j = 0; j < list2.length-1; j++) 
 		{
 			if(list2[j].compareTo(list2[j+1])>0) {
@@ -46,52 +47,64 @@ public class ArrayMethods2 {
 		}
 	
 		while(sorted1 && sorted2) {
+			combinedArray = new String[list1.length + list2.length];
 			int x=0;
 			int y=0;
-			for(int z=0; z< combinedArray.length; z++) { 
+			int z=0;
 			  while (x <list1.length && y < list2.length)
 		        {
 		            if (list1[x].compareTo(list2[y]) < 0)
 		            {
 		               combinedArray[z] = list1[x];
 		                x++;
+		                sorted1 =false;
+				   
 		            }
 		            else
 		            {
 		            	combinedArray[z] = list2[y];
 		                y++;
+				        sorted2 =false;
 		            }
 		            z++;
-		        }
-			  if (x < list1.length)
+		        	}
+			  
+			  while (x < list1.length)
 			    {
 			        combinedArray[z] = list1[x];
 			        x++;
 			        z++;
+			        sorted1 =false;
+			      
 			    }
 
-			    if (y < list2.length)
+			    while (y < list2.length)
 			    {
 			        combinedArray[z] = list2[y];
 			        y++;
 			        z++;
+			      
+			        sorted2 =false;
 			    }
 
-			    return combinedArray;  
+			    return combinedArray;
+		
+			    
 		}
-	}
 		return combinedArray;
-		
-		
 	}
+	
+
+	//http://www.dummies.com/programming/java/how-to-use-the-partition-method-for-quicksort-in-java/
+	
 	
 	public static int partition(int[]list) {
 		int low=0;
-		int high = 0;
+		int high = list.length;
 		 int i = low - 1;
-		 int j = high + 1;
+		 int j = high;
 		for(int x =0; x<list.length-1;x++) {
-		 high = list.length;
+		 high = list.length-1;
 		int pivot = list[x];
 		low = pivot;
 		 while (i < j)
