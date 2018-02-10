42a83337e555cd987b175755f991fd672b80f5b1
Author <PC@Dell>
2018-02-09 23:38:12 -0500

    commit

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
 
+/*
+ * Evan Wu
+ * AP CSA Period 2
+ * Lab 3.2
+ */
+
 public class ArrayMethods2 {
 
+	//starter code
 	public static void main(String[] args) {
 		String[] list1 = {"apple","cucumber","microsoft","zorro"};
 		String[] list2 = {"banana","cherry","mahogany","oreos", "pinata"};
@@ -11,7 +18,7 @@ public class ArrayMethods2 {
 		String[] mergeResult = merge(list1, list2);
 		long end = System.nanoTime();
 		long time = end - start;
-		System.out.println("M took: " + time + " nanoseconds");
+		System.out.println("Merge test took: " + time + " nanoseconds");
 		System.out.println(Arrays.toString(mergeResult));
 		
 		start = System.nanoTime();
@@ -24,9 +31,18 @@ public class ArrayMethods2 {
 	}
 	
 
-//https://stackoverflow.com/questions/5958169/how-to-merge-two-sorted-arrays-into-a-sorted-array
+//Sourced from: https://stackoverflow.com/questions/5958169/how-to-merge-two-sorted-arrays-into-a-sorted-array
 
 	public static String[] merge(String[] list1 , String[] list2) {
+		/*
+		 * This method will combine two pre-conditioned sorted arrays into one sorted arrays.
+		 * To do so, a new array must be created, which in this case is combinedArray.
+		 * To add the values into the arrays, the two arrays must be compared.
+		 * So in this case, if the comparing of two strings is less than zero, then the compared value will be added to the array first as it is smaller.
+		 * To find the values, there are two loops that loop through each of the two arrays if the values are less than the array length, as
+		 * going over will break it.
+		 * 
+		 */
 			String [] combinedArray = new String[list1.length + list2.length]; //new array must be length of both arrays combined
 			int x=0; //list1 indexes
 			int y=0; //list2 indexes
@@ -46,7 +62,7 @@ public class ArrayMethods2 {
 		            z++;
 		        	}
 			  		
-			  		//loops through the arrays
+			  		//loops through the arrays to find values
 			  		if (x < list1.length)
 			  		{
 			        combinedArray[z] = list1[x];
@@ -59,38 +75,43 @@ public class ArrayMethods2 {
 			        y++; z++;
 			  		}
 
-			    return combinedArray; //returns the combined sorted array
-		
-			    
+			    return combinedArray; //returns the combined sorted array 
 		}
 		
 	
-
+ 
 	public static int partition(int[]list) {
 		
-		int pivot = list[0];//pivot is the first value in the array
-		int i = 0;
 		int pivotLocation = 0;
-		int k = list.length;
-		for(int j = 1; j > list.length-1; j++) {
-		if (list[j] <= pivot)
+		int pivot = list[0];//pivot is the first value in the array
+		int k = list.length - 1; //last value in array
+		for(int j = 1; j < list.length; j++) { //loops through values after pivot
+		/*
+		 * While running the loop, any value j will get compared to the pivot. The pivot compares the value and if the value is less,
+		 * then value j will swapped with the closest value that is greater than the value of the pivot, which would be j-1. The 
+		 * pivot location will be at j since pivot is supposed to be between those values when sorted.
+		 * 
+		 * If j is greater than the pivot, then the larger value will swap will swap with the last value of the array as the values
+		 * that were found to be greater than pivot are pushed down and the opposite is true for smaller values.   
+		 *
+		 */
+		if (list[j] <= pivot) 
 		 {
-			swap(list, i, pivot);
-			pivotLocation  = i;
+			swap(list, j, j-1); 
+			pivotLocation = j;
 		 }
-		 if(list[j] > pivot)
+		 
+		else if(list[j] > pivot && k > j)
 		 {
 		
-			swap(list, i, pivot);
-			i++;
+			swap(list, j, k);
+			j--;
 			k--;
 		 }
 		 
 		}
 		 
-	
-	
-	return pivotLocation;	//returns the location of the pivot
+		return pivotLocation;	//returns the location/index of the pivot
 	}
 
 	//swaps values
