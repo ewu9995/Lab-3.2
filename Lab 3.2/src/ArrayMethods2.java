abd69cfe45d4755c70fd86478da52da5272fa707
Author <PC@Dell>
2018-02-09 19:16:21 -0500

    partition needs fixes

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -11,16 +11,16 @@ public class ArrayMethods2 {
 		String[] mergeResult = merge(list1, list2);
 		long end = System.nanoTime();
 		long time = end - start;
-		System.out.println("Test1 took: " + time + " nanoseconds");
+		System.out.println("M took: " + time + " nanoseconds");
 		System.out.println(Arrays.toString(mergeResult));
 		
 		start = System.nanoTime();
 		int pivotFinalPos = partition(list);
 		end = System.nanoTime();
 		time = end - start;
-		System.out.println("Test2 took: " + time + " nanoseconds");
+		System.out.println("Partition test took: " + time + " nanoseconds");
 		System.out.println("Final Pivot Position: " + pivotFinalPos);
-		System.out.println(list);
+		System.out.println(Arrays.toString(list));
 	}
 	
 
@@ -28,66 +28,72 @@ public class ArrayMethods2 {
 
 	public static String[] merge(String[] list1 , String[] list2) {
 			String [] combinedArray = new String[list1.length + list2.length]; //new array must be length of both arrays combined
-			int x=0; //list1
-			int y=0; //list2
+			int x=0; //list1 indexes
+			int y=0; //list2 indexes
 			int z=0; //new array (combinedArray)
-			  while (x < list1.length && y < list2.length)
+			  while (x < list1.length && y < list2.length) //both arrays must be less than their intended length (any longer with break the code)
 		        {
-		            if (list1[x].compareTo(list2[y]) < 0)
+		            if (list1[x].compareTo(list2[y]) < 0) //compares the values in the 2 arrays and see which is smaller
 		            {
-		               combinedArray[z] = list1[x];
+		               combinedArray[z] = list1[x]; //since the value in list1 is smaller in this case, list1[x] becomes part of the sorted array before list2[y]
 		                x++;
-		              
 		            }
 		            else
 		            {
-		            	combinedArray[z] = list2[y];
+		            	combinedArray[z] = list2[y]; //since list2[y] is smaller, it is placed before list1[x]
 		                y++; 
 		            }
 		            z++;
 		        	}
-			  
-			  	if (x < list1.length)
-			    {
+			  		
+			  		//loops through the arrays
+			  		if (x < list1.length)
+			  		{
 			        combinedArray[z] = list1[x];
 			        x++; z++;
-			    }
+			  		}
 
-			    if (y < list2.length)
-			    {
+			  		if (y < list2.length)
+			  		{
 			        combinedArray[z] = list2[y];
 			        y++; z++;
-			    }
+			  		}
 
-			    return combinedArray;
+			    return combinedArray; //returns the combined sorted array
 		
 			    
 		}
 		
 	
-	
 
-	//http://www.dummies.com/programming/java/how-to-use-the-partition-method-for-quicksort-in-java/
-	
-	
 	public static int partition(int[]list) {
+		
+		int pivot = list[0];//pivot is the first value in the array
 		int i = 0;
-		int j = list.length-1;
-		int pivot = list[0];
-		 while (i < j)
+		int pivotLocation = 0;
+		int k = list.length;
+		for(int j = 1; j > list.length-1; j++) {
+		if (list[j] <= pivot)
 		 {
-		  for (i++; list[i] < pivot; i++) {
-		  for (j--; list[j] > pivot; j--) {
-		  if (i < j)
-		   swap(list, i, j);
-		 
+			swap(list, i, pivot);
+			pivotLocation  = i;
 		 }
+		 if(list[j] > pivot)
+		 {
 		
-		 return j;
-		
+			swap(list, i, pivot);
+			i++;
+			k--;
+		 }
+		 
+		}
+		 
+	
+	
+	return pivotLocation;	//returns the location of the pivot
 	}
 
-
+	//swaps values
 	public static void swap(int []arr, int index1, int index2) {
 		int temp = arr[index1];
 		arr[index1] = arr[index2];
